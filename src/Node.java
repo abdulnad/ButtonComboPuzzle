c355ba44cd743e1fcb9677d8e2bd1298a4839721
abdulnad <abdullah7998@gmail.com>
2020-12-20 18:43:39 -0500

    new code

--- src/Node.java
+++ src/Node.java
@@ -2,14 +2,22 @@ public class Node {
     int pos;
     Node next;
     Node prev;
+    Boolean pressed = false;
 
 
     public Node(){
 
     }
-    public Node(int pos, Node next, Node prev){
+    public Node(int pos){
         this.pos = pos;
+        this.next = null;
+    }
+    public void addNext(Node next){
         this.next = next;
+    }
+    public void addPrev(Node prev){
         this.prev = prev;
     }
+
+
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[] arr = {0,1,2,3,4,5,6,7,8,9};
+        CircularLinkedList cl = new CircularLinkedList();
+        cl.displayList();
+        cl.solve();
     }
 }
