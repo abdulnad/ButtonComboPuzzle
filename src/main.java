947b2a34855cf18a38746252aedb4051be65f2b8
abdulnad <abdullah7998@gmail.com>
2020-12-21 18:11:18 -0500

    added buttonnode class

--- src/main.java
+++ src/main.java
@@ -1,11 +1,16 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.Arrays;
 import java.util.Collections;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public class main {
+public class main{
+    JButton button = new JButton();
+
     public static void main(String[] args) {
-        int[] arr = {0,1,2,3,4,5,6,7,8,9};
-        CircularLinkedList cl = new CircularLinkedList();
-        cl.displayList();
-        cl.solve();
+
     }
+
+
 }
